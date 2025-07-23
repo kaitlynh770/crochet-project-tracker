8b0df854b3e678eacd50f2f6131ea420c96bf7bf
Kaitlyn Huynh <huynhkaitlyn@meta.com>
2025-07-23 09:43:09 -0600

    changed name of allprojects scss file so that projects can be displayed in rows

--- src/pages/AllProjects.js
+++ src/pages/AllProjects.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Project from '../components/Project';
 import miffy from '../assets/miffy_keychain.jpeg'
 import my_melody from '../assets/my_melodys.jpg'
-import styles from '../pages_styling/AllProjects.scss'
+import styles from '../pages_styling/AllProjects.module.scss'
 function AllProjects(){
     const projects = [
         {
