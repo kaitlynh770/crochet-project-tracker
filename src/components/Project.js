aeff69a57281bd6c9c72cf361a340bbd58083b94
Kaitlyn Huynh <huynhkaitlyn@meta.com>
2025-07-23 15:47:32 -0600

    added buttons that'll toggle depending on state of details

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Piece from './Piece';
 import styles from './Project.module.scss'
 
 //this is going to be what a project is made up of
 //photo, name, pieces Piece[]
 function Project({projectImg, name, pieces}){
+    let [details, showDetails] = useState(false) //showing details of a project should initially be set to false
+    console.log(details)
     return(
         <div className = {styles.project}>
             <img src = {projectImg} alt = "Project Image"/>
             <h2 className = {styles.projectName}>
                 {name}
             </h2>
+            <button className= {styles.toggleButton} onClick = {() => showDetails(!details)}>{details ? '▲': '▼' }</button>
             <div className= {styles.project_details}>
                 {pieces.map((piece, index) => (
                     <Piece key = {index} name = {piece.name} rounds = {piece.rounds} quantity={piece.quantity}/>
