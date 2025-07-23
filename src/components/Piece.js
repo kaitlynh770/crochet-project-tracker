fb484810c52674986c75711c43890398809c0f8a
Kaitlyn Huynh <huynhkaitlyn@meta.com>
2025-07-23 14:38:16 -0600

    finished styling card for projects

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import styles from './Piece.module.scss'
 //each project is made up of multiple pieces
 //a piece should be made up of the name (arm, leg, ear) and how many rounds it is (8 rounds total, 16 rounds total, etc)
 function Piece({name, rounds, quantity = 1}){
     return(
-        <div>
+        <div className = {styles.piece}>
             <span>
-                <h2>{name}: {quantity > 1 ? `x${quantity}` : ''} </h2>
-                <p>{rounds} rounds</p>
+                <span className = {styles.pieceName}>{name}{quantity > 1 ? ` x${quantity}` : ''}:  </span>
+                <span className = {styles.pieceRounds}>{rounds} rounds</span>
             </span>
         </div>
     );
--- src/components/Project.js
+++ src/components/Project.js
@@ -8,12 +8,14 @@ function Project({projectImg, name, pieces}){
     return(
         <div className = {styles.project}>
             <img src = {projectImg} alt = "Project Image"/>
-            <h2>
+            <h2 className = {styles.projectName}>
                 {name}
             </h2>
-            {pieces.map((piece, index) => (
-                <Piece key = {index} name = {piece.name} rounds = {piece.rounds} quantity={piece.quantity}/>
-            ))}
+            <div className= {styles.project_details}>
+                {pieces.map((piece, index) => (
+                    <Piece key = {index} name = {piece.name} rounds = {piece.rounds} quantity={piece.quantity}/>
+                ))}
+            </div>
         </div>
     )
 }
