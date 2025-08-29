5e814c025601464da858c4818bc5b429d4df0770
Kaitlyn Huynh <huynhkaitlyn@meta.com>
2025-08-29 15:05:18 -0600

    adjusted sizing so that more projects could fit on one line

--- src/pages/AllProjects.js
+++ src/pages/AllProjects.js
@@ -4,45 +4,6 @@ import miffy from '../assets/miffy_keychain.jpeg'
 import my_melody from '../assets/my_melodys.jpg'
 import styles from '../pages_styling/AllProjects.module.scss'
 function AllProjects({projects}){
-    // const projects = [
-    //     {
-    //         id: 1,
-    //         name: 'Tulip Musie Bunnie',
-    //         projectImg: miffy,
-    //         pieces: [
-    //             {
-    //                 name: 'Head',
-    //                 rounds: 10,
-    //             },
-    //             {
-    //                 name: 'Ears',
-    //                 rounds: 6,
-    //                 quantity: 2
-    //             }
-    //         ]
-    //     },
-    //     {
-    //         id: 2,
-    //         name: 'Winter My Melody',
-    //         projectImg: my_melody,
-    //         pieces: [
-    //             {
-    //                 name: 'Head',
-    //                 rounds: 15
-    //             },
-    //             {
-    //                 name: 'Arms',
-    //                 rounds: 8,
-    //                 quantity: 2
-    //             },
-    //             {
-    //                 name: 'Ears',
-    //                 rounds: 12,
-    //                 quantity: 2
-    //             }
-    //         ]
-    //     }
-    // ]
     return(
         <div>
             <h1>
