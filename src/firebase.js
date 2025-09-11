eb27506731efbf34bf30cdae2a5ec7e1be1e66d9
Huynh <kaitlyn.huynh@accenture.com>
2025-09-11 15:27:03 -0600

    added info regarding the firebase info

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  // ...other config values from Firebase Console
+  apiKey: "AIzaSyBf8sJJEyarSGu3S2GwCU_x8IYzc_PqW-4",
+  authDomain: "crochet-project-tracker-a62ba.firebaseapp.com",
+  databaseURL: "https://crochet-project-tracker-a62ba-default-rtdb.firebaseio.com",
+  projectId: "crochet-project-tracker-a62ba",
+  storageBucket: "crochet-project-tracker-a62ba.firebasestorage.app",
+  messagingSenderId: "400986894867",
+  appId: "1:400986894867:web:c6082cd2f707e783c7c5e6",
+  measurementId: "G-8Z64T3XCW0"
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
