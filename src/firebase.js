4bc2ce53d152b71e873c7c3652cbd69208e3bafd
Kaitlyn Huynh <huynhkaitlyn@meta.com>
2025-09-11 13:56:33 -0600

    added firebase file

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  // ...other config values from Firebase Console
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
