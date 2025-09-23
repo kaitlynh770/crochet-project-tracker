671a158c156f43a66513e20d95e1665b898428b2
Kaitlyn Huynh <huynhkaitlyn@meta.com>
2025-09-23 11:44:12 -0600

    did some stuff with hiding api

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBf8sJJEyarSGu3S2GwCU_x8IYzc_PqW-4",
+//   authDomain: "crochet-project-tracker-a62ba.firebaseapp.com",
+//   databaseURL: "https://crochet-project-tracker-a62ba-default-rtdb.firebaseio.com",
+//   projectId: "crochet-project-tracker-a62ba",
+//   storageBucket: "crochet-project-tracker-a62ba.firebasestorage.app",
+//   messagingSenderId: "400986894867",
+//   appId: "1:400986894867:web:c6082cd2f707e783c7c5e6",
+//   measurementId: "G-8Z64T3XCW0"
+// };
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
