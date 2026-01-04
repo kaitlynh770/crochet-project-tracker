c36903cf6d70c00858cf2ad480833b6957fe1877
Kaitlyn Huynh <kahu1074@colorado.edu>
2026-01-03 22:42:18 -0700

    edit notes section's font size from 12px -> 16px

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,7 +4,7 @@ export function Notes(props) {
       <h2>Notes:</h2>
       {/* the font is different here, but these get saved as a bulleted list already, so there's no need to format.
        There shouldn't be any downsides to this since it's already accessible and SEO isn't a concern with user generated content.   */}
-      <pre style={{ textAlign: 'left' }}>{props.notes}</pre>
+      <pre style={{ textAlign: 'left', fontSize: '16px' }}>{props.notes}</pre>
     </div>
   );
 }
